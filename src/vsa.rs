8f8f739962b8d76a56a397a8816686f5e55db0ca
Mikail Khan <mikail@mikail-khan.com>
2023-01-30 17:13:49 -0500

    more vsa outline

--- src/vsa.rs
+++ src/vsa.rs
@@ -1,13 +1,15 @@
+use std::rc::Rc;
+
 pub trait Language<L> {
     fn eval(&self, args: &[L]) -> L;
 }
 
 pub enum VSA<L: Clone, F: Language<L>> {
     Leaf(Vec<L>),
-    Union(Vec<VSA<L, F>>),
+    Union(Vec<Rc<VSA<L, F>>>),
     Join { 
         op: F,
-        children: Vec<VSA<L, F>>,
+        children: Vec<Rc<VSA<L, F>>>,
     }
 }
 
@@ -17,10 +19,22 @@ impl<L: Clone, F: Language<L>> VSA<L, F> {
             VSA::Leaf(c) => c[0].clone(),
             VSA::Union(c) => c[0].eval(),
             VSA::Join { op, children } => {
-                let cs = children.iter().map(VSA::eval).collect::<Vec<_>>();
+                let cs = children.iter().map(|vsa| vsa.clone().eval()).collect::<Vec<_>>();
                 op.eval(&cs)
             }
         }
+    }
+
+    fn intersect(&self, other: &VSA<L, F>) -> VSA<L, F> {
+        todo!()
+    }
+
+    fn ranking(&self, cmp: impl Fn(&VSA<L, F>) -> usize) -> VSA<L, F> {
+        todo!()
+    }
+
+    fn cluster(&self, examples: &Vec<(L, L)>) -> Vec<(L, VSA<L, F>)> {
+        todo!()
     }
 }
 
