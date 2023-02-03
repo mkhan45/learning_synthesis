1c6a27a3c9cb46d64d2c51a610525db9b6ef43d3
Mikail Khan <mikail@mikail-khan.com>
2023-02-03 01:00:34 -0500

    vsa intersect?

--- src/vsa.rs
+++ src/vsa.rs
@@ -1,19 +1,19 @@
-use std::{collections::HashSet, rc::Rc};
+use std::{collections::HashSet, marker::PhantomData, rc::Rc};
 
 pub trait Language<L> {
     fn eval(&self, args: &[L]) -> L;
 }
 
-pub enum VSA<L: Clone + Eq + std::hash::Hash, F: Language<L>> {
-    Leaf(HashSet<Rc<L>>),
+pub enum VSA<L: Clone + Eq + std::hash::Hash, F: Language<L> + std::hash::Hash> {
+    Leaf(HashSet<Rc<AST<L, F>>>),
     Union(Vec<Rc<VSA<L, F>>>),
     Join { op: F, children: Vec<Rc<VSA<L, F>>> },
 }
 
-impl<L: Clone + Eq + std::hash::Hash, F: Language<L> + Eq + Copy> VSA<L, F> {
+impl<L: Clone + Eq + std::hash::Hash, F: Language<L> + Eq + Copy + std::hash::Hash> VSA<L, F> {
     fn eval(&self) -> L {
         match self {
-            VSA::Leaf(c) => c.iter().next().unwrap().clone().as_ref().clone(),
+            VSA::Leaf(c) => c.iter().next().unwrap().clone().eval(),
             VSA::Union(c) => c[0].eval(),
             VSA::Join { op, children } => {
                 let cs = children
@@ -22,6 +22,20 @@ impl<L: Clone + Eq + std::hash::Hash, F: Language<L> + Eq + Copy> VSA<L, F> {
                     .collect::<Vec<_>>();
                 op.eval(&cs)
             }
+        }
+    }
+
+    fn contains(&self, program: &AST<L, F>) -> bool {
+        match self {
+            VSA::Leaf(s) => s.contains(program),
+            VSA::Union(vss) => vss.iter().any(|vs| vs.contains(program)),
+            VSA::Join { op, children } => match program.fun {
+                Some(f) if f == *op => program
+                    .args
+                    .iter()
+                    .all(|arg| children.iter().any(|vss| vss.contains(arg))),
+                _ => false,
+            },
         }
     }
 
@@ -49,7 +63,13 @@ impl<L: Clone + Eq + std::hash::Hash, F: Language<L> + Eq + Copy> VSA<L, F> {
 
             #[rustfmt::skip]
             (VSA::Join { op, children }, VSA::Leaf(s)) | (VSA::Leaf(s), VSA::Join { op, children })
-                => VSA::Leaf(todo!()),
+                => VSA::Leaf(s.iter().filter(|pj| {
+                    match pj.fun {
+                        Some(f) if f == *op => 
+                            pj.args.iter().all(|arg| children.iter().any(|cs| cs.clone().contains(arg))),
+                        _ => false,
+                    }
+                }).cloned().collect()),
 
             (VSA::Leaf(l_set), VSA::Leaf(r_set)) => {
                 VSA::Leaf(l_set.intersection(r_set).cloned().collect())
@@ -66,17 +86,25 @@ impl<L: Clone + Eq + std::hash::Hash, F: Language<L> + Eq + Copy> VSA<L, F> {
     }
 }
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 pub enum Fun {
     Concat,
     Find,
     Slice,
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub enum Lit {
     StringConst(String),
     LocConst(usize),
     LocEnd,
+}
+
+#[derive(Clone, Hash, PartialEq, Eq)]
+pub struct AST<L: std::hash::Hash, F: Language<L> + std::hash::Hash> {
+    pub fun: Option<F>,
+    pub args: Vec<AST<L, F>>,
+    phantom_l: PhantomData<L>,
 }
 
 impl Language<Lit> for Fun {
@@ -109,6 +137,16 @@ impl Language<Lit> for Fun {
                 [Lit::StringConst(_), Lit::LocEnd, _] => Lit::StringConst("".to_owned()),
                 _ => panic!(),
             },
+        }
+    }
+}
+
+impl<L: Clone + std::hash::Hash, F: Language<L> + Copy + std::hash::Hash> AST<L, F> {
+    pub fn eval(&self) -> L {
+        let evaled = self.args.iter().map(AST::eval).collect::<Vec<_>>();
+        match self.fun {
+            Some(fun) => fun.eval(&evaled),
+            None => evaled[0].clone(),
         }
     }
 }
