69fc8895e0ce228254873e2a3d0180c6751481bb
Mikail Khan <mikail@mikail-khan.com>
2023-02-28 14:36:23 -0500

    actually fixed base

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/learning-synthesis',
+  base: '/learning_synthesis',
   build: {
       outDir: '../docs',
   }
